f746c9fce0987dbfc144cc767a16b5a0f0f983f7
Robin <87436238+epi55@users.noreply.github.com>
2023-10-27 17:09:43 -0700

    Add files via upload

---
+++ salary.py
@@ -0,0 +1,13 @@
+# ENGINE SETUP
+def varEngine(annual, tma):
+    biweekly = (annual / 26) + tma
+    annualTMA = biweekly * 26
+    biweeklyNet = biweekly * net
+    print("###############\n# Annual: ${:.2f}\n# TMA: ${:.2f}\n#\n# Annual (with TMA): ${:.2f}\n# Biweekly (with TMA): ${:.2f}\n###############".format(annual, tma, annualTMA, biweekly))
+    print("# Biweekly (net): ${:.2f}\n###############".format(biweeklyNet))
+
+# RUN
+annual = 100665.30
+tma = 254.66
+net = 0.655
+varEngine(annual, tma)
