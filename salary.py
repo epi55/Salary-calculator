9380a8aea773bbb285b55111da93c3b57ce86049
Robin <87436238+epi55@users.noreply.github.com>
2023-12-06 10:40:04 -0800

    Add files via upload

--- salary.py
+++ salary.py
@@ -1,13 +1,24 @@
 # ENGINE SETUP
-def varEngine(annual, tma):
-    biweekly = (annual / 26) + tma
-    annualTMA = biweekly * 26
+data = {"PS27": [91992.70, 7666.06, 3526.07, 116.36, 50.3724], "IS27": [91992.70, 7666.06, 3526.07, 349.08, 50.3724], "IS30": [100665.30, 8388.78, 3858.49, 381.99, 55.1213],}
+
+def salaryEngine(data, id, net):
+    biweekly = data[id][2]
+    #biweeklyTMA = (data[id][2]) + data[id][3]
+    biweeklyTMA = (data[id][0] / 26) + data[id][3]
+    annual = data[id][0]
+    annualTMA = biweeklyTMA * 26
+    annualNet = annualTMA * net
     biweeklyNet = biweekly * net
-    print("###############\n# Annual: ${:.2f}\n# TMA: ${:.2f}\n#\n# Annual (with TMA): ${:.2f}\n# Biweekly (with TMA): ${:.2f}\n###############".format(annual, tma, annualTMA, biweekly))
-    print("# Biweekly (net): ${:.2f}\n###############".format(biweeklyNet))
+    print("\n")
+    print("# {}".format(id))
+    print("Biweekly + TMA: ${:.2f}".format(biweeklyTMA))
+    print("Annual + TMA: ${:.2f}".format(annualTMA))
+    print("Biweekly + TMA + Net: ${:.2f}".format(biweeklyNet))
+    print("Annual + TMA + Net: ${:.2f}".format(annualNet))
 
 # RUN
-annual = 0
-tma = 0
-net = 0.655
-varEngine(annual, tma)
+id = "PS27"
+net = (0.655)
+salaryEngine(data, id, net)
+salaryEngine(data, "IS27", net)
+salaryEngine(data, "IS30", net)
