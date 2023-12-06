8d87cbb6dd529e56022ad51cda2a37886f096307
Robin <87436238+epi55@users.noreply.github.com>
2023-12-06 10:56:20 -0800

    Add files via upload

--- salary.py
+++ salary.py
@@ -1,24 +1,26 @@
 # ENGINE SETUP
-data = {"PS27": [91992.70, 7666.06, 3526.07, 116.36, 50.3724], "IS27": [91992.70, 7666.06, 3526.07, 349.08, 50.3724], "IS30": [100665.30, 8388.78, 3858.49, 381.99, 55.1213],}
+data = {"21": [77012.22, 6417.69, 2951.87, 42.1696], "24": [84134.34, 7011.20, 3224.86, 46.0694], "27": [91992.70, 7666.06, 3526.07, 50.3724], "30": [100665.30, 8388.78, 3858.49, 55.1213]}
+tma = {"0": 0.000, "3": 0.033, "6": 0.066, "9": 0.099 }
 
-def salaryEngine(data, id, net):
+def salaryEngine(data, id, tma, rate, net):
     biweekly = data[id][2]
-    #biweeklyTMA = (data[id][2]) + data[id][3]
-    biweeklyTMA = (data[id][0] / 26) + data[id][3]
+    biweeklyTMA = (data[id][0] / 26) + (data[id][2] * tma[rate])
+    #biweeklyTMA = (data[id][2]) + (data[id][2] * tma[rate]) ## This is (quoted biweekly); using (quoted annual / 26).
     annual = data[id][0]
     annualTMA = biweeklyTMA * 26
     annualNet = annualTMA * net
     biweeklyNet = biweekly * net
     print("\n")
-    print("# {}".format(id))
+    print("# {} + {}%".format(id, rate))
     print("Biweekly + TMA: ${:.2f}".format(biweeklyTMA))
     print("Annual + TMA: ${:.2f}".format(annualTMA))
     print("Biweekly + TMA + Net: ${:.2f}".format(biweeklyNet))
     print("Annual + TMA + Net: ${:.2f}".format(annualNet))
 
 # RUN
-id = "PS27"
+id = "27" # 21 / 24 / 27/ 30
+rate = "3" # 0 / 3 / 6 / 9
 net = (0.655)
-salaryEngine(data, id, net)
-salaryEngine(data, "IS27", net)
-salaryEngine(data, "IS30", net)
+salaryEngine(data, id, tma, rate, net)
+salaryEngine(data, "27", tma, "9", net)
+salaryEngine(data, "30", tma, "9", net)
